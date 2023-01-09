4dfcbde82bcf580ff7f88151c34a6f601b6d2732
Daniel Shechter <dshctr1993@gmail.com>
2023-01-09 16:41:54 +0200

    uploaed to render attempt 2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('*')
+  sendIndex(req: any, res: { sendFile: (arg0: string) => void }) {
+    res.sendFile(join(__dirname, 'client', 'index.html'));
   }
 }
+// import { Controller, Get } from '@nestjs/common';
+// import { AppService } from './app.service';
+
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
