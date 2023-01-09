ab4a84a164dda1dd979ff3f76c38e5982c962ab3
Daniel Shechter <dshctr1993@gmail.com>
2023-01-09 08:47:38 +0200

    uploaed to render

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,24 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
 import { MessageModule } from './message/message.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
+// const connectionString1 = process.env.NODE_ENV === 'production'
+//   ? process.env.MONGODB_URI_PROD
+//   : process.env.MONGODB_URI
 const connectionString = process.env.NODE_ENV === 'production'
   ? 'mongodb+srv://ndh:ndhndh@cluster0.t7f0r.mongodb.net/?retryWrites=true&w=majority'
   : 'mongodb://localhost:27017/whosapp_db';
 
+  console.log(process.env);
+  
+
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
     MongooseModule.forRoot(connectionString),
     UsersModule,
     AuthModule,
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 export class SocketAdapter extends IoAdapter {
   createIOServer(
     port: number,
@@ -17,7 +19,7 @@ export class SocketAdapter extends IoAdapter {
     const server = super.createIOServer(port, {
       ...options,
       cors: {
-        origin: 'http://127.0.0.1:5173',
+        origin: ['http://127.0.0.1:5173', 'https://whosapp.onrender.com/'],
         methods: ['GET', 'POST'],
       },
     });
@@ -27,12 +29,13 @@ export class SocketAdapter extends IoAdapter {
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
- app.useStaticAssets(join(__dirname, 'client')); 
-  // app.useStaticAssets(join(__dirname, '..', 'static'));
+  app.useStaticAssets(join(__dirname, '..', 'client'));
   app.enableCors({
-    origin: 'http://127.0.0.1:5173',
+    origin: ['http://127.0.0.1:5173', 'https://whosapp.onrender.com/']
   });
   app.useWebSocketAdapter(new SocketAdapter(app));
+  
   await app.listen(3001);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap()
